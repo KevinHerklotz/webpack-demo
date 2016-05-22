71a47d3be2f2ff2e634aa0c7bec8ee19985ad82c
Kevin Herklotz <kh@move-elevator.de>
2016-05-22 08:53:04 +0200

    fixed wrong letter

--- 1-bundle/module-a.js
+++ 1-bundle/module-a.js
@@ -1,4 +1,4 @@
 var moduleB = require('./module-b.js');
 
 console.log('module-a.js');
-document.write('<div class="moduleA">module B</div>');
+document.write('<div class="moduleA">module A</div>');
